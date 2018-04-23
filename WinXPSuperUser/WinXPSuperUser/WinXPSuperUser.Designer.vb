d606ba91197de4433f2500e31da28e70974e3a60
Reece Callister <37028737+ReecesPuffs@users.noreply.github.com>
2018-04-23 20:53:22 +0100

    First And probs only update
    (Hopefully should be no issues and no bugs) Check the site
    https://reecespuffs.github.io/web

---
+++ WinXPSuperUser/WinXPSuperUser/WinXPSuperUser.Designer.vb
@@ -0,0 +1,378 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class WinXPSuperUser
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(WinXPSuperUser))
+        Me.GroupPolicyEditor = New System.Windows.Forms.Button()
+        Me.AppWizard = New System.Windows.Forms.Button()
+        Me.Verify = New System.Windows.Forms.Button()
+        Me.AbortShutdown = New System.Windows.Forms.Button()
+        Me.Appdata = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.About = New System.Windows.Forms.Button()
+        Me.DiskCleanup = New System.Windows.Forms.Button()
+        Me.CompMgmt = New System.Windows.Forms.Button()
+        Me.Services = New System.Windows.Forms.Button()
+        Me.Winver = New System.Windows.Forms.Button()
+        Me.DirectXDiag = New System.Windows.Forms.Button()
+        Me.DeviceManager = New System.Windows.Forms.Button()
+        Me.EventViewer = New System.Windows.Forms.Button()
+        Me.Restart = New System.Windows.Forms.Button()
+        Me.Shutdown = New System.Windows.Forms.Button()
+        Me.Logoff = New System.Windows.Forms.Button()
+        Me.ControlPanel = New System.Windows.Forms.Button()
+        Me.TaskManager = New System.Windows.Forms.Button()
+        Me.CMD = New System.Windows.Forms.Button()
+        Me.Regedit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'GroupPolicyEditor
+        '
+        Me.GroupPolicyEditor.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.GroupPolicyEditor.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.GroupPolicyEditor.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.GroupPolicyEditor.Image = CType(resources.GetObject("GroupPolicyEditor.Image"), System.Drawing.Image)
+        Me.GroupPolicyEditor.Location = New System.Drawing.Point(125, 47)
+        Me.GroupPolicyEditor.Name = "GroupPolicyEditor"
+        Me.GroupPolicyEditor.Size = New System.Drawing.Size(92, 23)
+        Me.GroupPolicyEditor.TabIndex = 55
+        Me.GroupPolicyEditor.Text = "GroupPolicyEditor"
+        Me.GroupPolicyEditor.UseVisualStyleBackColor = True
+        '
+        'AppWizard
+        '
+        Me.AppWizard.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.AppWizard.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.AppWizard.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.AppWizard.Image = CType(resources.GetObject("AppWizard.Image"), System.Drawing.Image)
+        Me.AppWizard.Location = New System.Drawing.Point(125, 193)
+        Me.AppWizard.Name = "AppWizard"
+        Me.AppWizard.Size = New System.Drawing.Size(92, 23)
+        Me.AppWizard.TabIndex = 53
+        Me.AppWizard.Text = "App Wizard"
+        Me.AppWizard.UseVisualStyleBackColor = True
+        '
+        'Verify
+        '
+        Me.Verify.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Verify.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Verify.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Verify.Image = CType(resources.GetObject("Verify.Image"), System.Drawing.Image)
+        Me.Verify.Location = New System.Drawing.Point(247, 4)
+        Me.Verify.Name = "Verify"
+        Me.Verify.Size = New System.Drawing.Size(92, 23)
+        Me.Verify.TabIndex = 52
+        Me.Verify.Text = "Verify"
+        Me.Verify.UseVisualStyleBackColor = True
+        '
+        'AbortShutdown
+        '
+        Me.AbortShutdown.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.AbortShutdown.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.AbortShutdown.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.AbortShutdown.Image = CType(resources.GetObject("AbortShutdown.Image"), System.Drawing.Image)
+        Me.AbortShutdown.Location = New System.Drawing.Point(247, 105)
+        Me.AbortShutdown.Name = "AbortShutdown"
+        Me.AbortShutdown.Size = New System.Drawing.Size(92, 23)
+        Me.AbortShutdown.TabIndex = 50
+        Me.AbortShutdown.Text = "Abort Shutdown"
+        Me.AbortShutdown.UseVisualStyleBackColor = True
+        '
+        'Appdata
+        '
+        Me.Appdata.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Appdata.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Appdata.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Appdata.Image = CType(resources.GetObject("Appdata.Image"), System.Drawing.Image)
+        Me.Appdata.Location = New System.Drawing.Point(8, 105)
+        Me.Appdata.Name = "Appdata"
+        Me.Appdata.Size = New System.Drawing.Size(93, 23)
+        Me.Appdata.TabIndex = 49
+        Me.Appdata.Text = "Appdata"
+        Me.Appdata.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.WindowText
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.TextBox1.ForeColor = System.Drawing.SystemColors.Window
+        Me.TextBox1.Location = New System.Drawing.Point(140, 7)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(92, 20)
+        Me.TextBox1.TabIndex = 47
+        Me.TextBox1.Text = "C"
+        '
+        'About
+        '
+        Me.About.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.About.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.About.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.About.Image = CType(resources.GetObject("About.Image"), System.Drawing.Image)
+        Me.About.Location = New System.Drawing.Point(6, 7)
+        Me.About.Name = "About"
+        Me.About.Size = New System.Drawing.Size(93, 23)
+        Me.About.TabIndex = 46
+        Me.About.Text = "About"
+        Me.About.UseVisualStyleBackColor = True
+        '
+        'DiskCleanup
+        '
+        Me.DiskCleanup.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.DiskCleanup.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.DiskCleanup.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.DiskCleanup.Image = CType(resources.GetObject("DiskCleanup.Image"), System.Drawing.Image)
+        Me.DiskCleanup.Location = New System.Drawing.Point(9, 194)
+        Me.DiskCleanup.Name = "DiskCleanup"
+        Me.DiskCleanup.Size = New System.Drawing.Size(93, 23)
+        Me.DiskCleanup.TabIndex = 45
+        Me.DiskCleanup.Text = "DiskCleanup"
+        Me.DiskCleanup.UseVisualStyleBackColor = True
+        '
+        'CompMgmt
+        '
+        Me.CompMgmt.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.CompMgmt.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.CompMgmt.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.CompMgmt.Image = CType(resources.GetObject("CompMgmt.Image"), System.Drawing.Image)
+        Me.CompMgmt.Location = New System.Drawing.Point(125, 105)
+        Me.CompMgmt.Name = "CompMgmt"
+        Me.CompMgmt.Size = New System.Drawing.Size(93, 23)
+        Me.CompMgmt.TabIndex = 44
+        Me.CompMgmt.Text = "CompMgmt"
+        Me.CompMgmt.UseVisualStyleBackColor = True
+        '
+        'Services
+        '
+        Me.Services.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Services.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Services.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Services.Image = CType(resources.GetObject("Services.Image"), System.Drawing.Image)
+        Me.Services.Location = New System.Drawing.Point(8, 76)
+        Me.Services.Name = "Services"
+        Me.Services.Size = New System.Drawing.Size(93, 23)
+        Me.Services.TabIndex = 43
+        Me.Services.Text = "Services"
+        Me.Services.UseVisualStyleBackColor = True
+        '
+        'Winver
+        '
+        Me.Winver.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Winver.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Winver.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Winver.Image = CType(resources.GetObject("Winver.Image"), System.Drawing.Image)
+        Me.Winver.Location = New System.Drawing.Point(8, 47)
+        Me.Winver.Name = "Winver"
+        Me.Winver.Size = New System.Drawing.Size(93, 23)
+        Me.Winver.TabIndex = 42
+        Me.Winver.Text = "Winver"
+        Me.Winver.UseVisualStyleBackColor = True
+        '
+        'DirectXDiag
+        '
+        Me.DirectXDiag.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.DirectXDiag.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.DirectXDiag.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.DirectXDiag.Image = CType(resources.GetObject("DirectXDiag.Image"), System.Drawing.Image)
+        Me.DirectXDiag.Location = New System.Drawing.Point(8, 135)
+        Me.DirectXDiag.Name = "DirectXDiag"
+        Me.DirectXDiag.Size = New System.Drawing.Size(93, 23)
+        Me.DirectXDiag.TabIndex = 39
+        Me.DirectXDiag.Text = "DxDiag"
+        Me.DirectXDiag.UseVisualStyleBackColor = True
+        '
+        'DeviceManager
+        '
+        Me.DeviceManager.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.DeviceManager.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.DeviceManager.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.DeviceManager.Image = CType(resources.GetObject("DeviceManager.Image"), System.Drawing.Image)
+        Me.DeviceManager.Location = New System.Drawing.Point(125, 134)
+        Me.DeviceManager.Name = "DeviceManager"
+        Me.DeviceManager.Size = New System.Drawing.Size(93, 23)
+        Me.DeviceManager.TabIndex = 38
+        Me.DeviceManager.Text = "DeviceManager"
+        Me.DeviceManager.UseVisualStyleBackColor = True
+        '
+        'EventViewer
+        '
+        Me.EventViewer.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.EventViewer.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.EventViewer.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.EventViewer.Image = CType(resources.GetObject("EventViewer.Image"), System.Drawing.Image)
+        Me.EventViewer.Location = New System.Drawing.Point(125, 164)
+        Me.EventViewer.Name = "EventViewer"
+        Me.EventViewer.Size = New System.Drawing.Size(92, 23)
+        Me.EventViewer.TabIndex = 37
+        Me.EventViewer.Text = "EventViewer"
+        Me.EventViewer.UseVisualStyleBackColor = True
+        '
+        'Restart
+        '
+        Me.Restart.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Restart.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Restart.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Restart.Image = CType(resources.GetObject("Restart.Image"), System.Drawing.Image)
+        Me.Restart.Location = New System.Drawing.Point(247, 194)
+        Me.Restart.Name = "Restart"
+        Me.Restart.Size = New System.Drawing.Size(92, 23)
+        Me.Restart.TabIndex = 36
+        Me.Restart.Text = "Restart"
+        Me.Restart.UseVisualStyleBackColor = True
+        '
+        'Shutdown
+        '
+        Me.Shutdown.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Shutdown.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Shutdown.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Shutdown.Image = CType(resources.GetObject("Shutdown.Image"), System.Drawing.Image)
+        Me.Shutdown.Location = New System.Drawing.Point(247, 134)
+        Me.Shutdown.Name = "Shutdown"
+        Me.Shutdown.Size = New System.Drawing.Size(92, 23)
+        Me.Shutdown.TabIndex = 35
+        Me.Shutdown.Text = "Shutdown"
+        Me.Shutdown.UseVisualStyleBackColor = True
+        '
+        'Logoff
+        '
+        Me.Logoff.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Logoff.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Logoff.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Logoff.Image = CType(resources.GetObject("Logoff.Image"), System.Drawing.Image)
+        Me.Logoff.Location = New System.Drawing.Point(247, 163)
+        Me.Logoff.Name = "Logoff"
+        Me.Logoff.Size = New System.Drawing.Size(92, 23)
+        Me.Logoff.TabIndex = 34
+        Me.Logoff.Text = "Logoff"
+        Me.Logoff.UseVisualStyleBackColor = True
+        '
+        'ControlPanel
+        '
+        Me.ControlPanel.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.ControlPanel.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ControlPanel.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.ControlPanel.Image = CType(resources.GetObject("ControlPanel.Image"), System.Drawing.Image)
+        Me.ControlPanel.Location = New System.Drawing.Point(9, 164)
+        Me.ControlPanel.Name = "ControlPanel"
+        Me.ControlPanel.Size = New System.Drawing.Size(92, 23)
+        Me.ControlPanel.TabIndex = 33
+        Me.ControlPanel.Text = "Control Panel"
+        Me.ControlPanel.UseVisualStyleBackColor = True
+        '
+        'TaskManager
+        '
+        Me.TaskManager.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.TaskManager.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.TaskManager.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.TaskManager.Image = CType(resources.GetObject("TaskManager.Image"), System.Drawing.Image)
+        Me.TaskManager.Location = New System.Drawing.Point(247, 47)
+        Me.TaskManager.Name = "TaskManager"
+        Me.TaskManager.Size = New System.Drawing.Size(92, 23)
+        Me.TaskManager.TabIndex = 32
+        Me.TaskManager.Text = "Task Manager"
+        Me.TaskManager.UseVisualStyleBackColor = True
+        '
+        'CMD
+        '
+        Me.CMD.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.CMD.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.CMD.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.CMD.Image = CType(resources.GetObject("CMD.Image"), System.Drawing.Image)
+        Me.CMD.Location = New System.Drawing.Point(125, 76)
+        Me.CMD.Name = "CMD"
+        Me.CMD.Size = New System.Drawing.Size(92, 23)
+        Me.CMD.TabIndex = 31
+        Me.CMD.Text = "CMD"
+        Me.CMD.UseVisualStyleBackColor = True
+        '
+        'Regedit
+        '
+        Me.Regedit.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Regedit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Regedit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Regedit.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Regedit.Image = CType(resources.GetObject("Regedit.Image"), System.Drawing.Image)
+        Me.Regedit.Location = New System.Drawing.Point(247, 76)
+        Me.Regedit.Name = "Regedit"
+        Me.Regedit.Size = New System.Drawing.Size(92, 23)
+        Me.Regedit.TabIndex = 30
+        Me.Regedit.Text = "Regedit"
+        Me.Regedit.UseVisualStyleBackColor = True
+        '
+        'WinXPSuperUser
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(345, 221)
+        Me.Controls.Add(Me.GroupPolicyEditor)
+        Me.Controls.Add(Me.AppWizard)
+        Me.Controls.Add(Me.Verify)
+        Me.Controls.Add(Me.AbortShutdown)
+        Me.Controls.Add(Me.Appdata)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.About)
+        Me.Controls.Add(Me.DiskCleanup)
+        Me.Controls.Add(Me.CompMgmt)
+        Me.Controls.Add(Me.Services)
+        Me.Controls.Add(Me.Winver)
+        Me.Controls.Add(Me.DirectXDiag)
+        Me.Controls.Add(Me.DeviceManager)
+        Me.Controls.Add(Me.EventViewer)
+        Me.Controls.Add(Me.Restart)
+        Me.Controls.Add(Me.Shutdown)
+        Me.Controls.Add(Me.Logoff)
+        Me.Controls.Add(Me.ControlPanel)
+        Me.Controls.Add(Me.TaskManager)
+        Me.Controls.Add(Me.CMD)
+        Me.Controls.Add(Me.Regedit)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "WinXPSuperUser"
+        Me.Text = "WinXPSuperUser"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupPolicyEditor As Button
+    Friend WithEvents AppWizard As Button
+    Friend WithEvents Verify As Button
+    Friend WithEvents AbortShutdown As Button
+    Friend WithEvents Appdata As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents About As Button
+    Friend WithEvents DiskCleanup As Button
+    Friend WithEvents CompMgmt As Button
+    Friend WithEvents Services As Button
+    Friend WithEvents Winver As Button
+    Friend WithEvents DirectXDiag As Button
+    Friend WithEvents DeviceManager As Button
+    Friend WithEvents EventViewer As Button
+    Friend WithEvents Restart As Button
+    Friend WithEvents Shutdown As Button
+    Friend WithEvents Logoff As Button
+    Friend WithEvents ControlPanel As Button
+    Friend WithEvents TaskManager As Button
+    Friend WithEvents CMD As Button
+    Friend WithEvents Regedit As Button
+End Class
---
+++ WinXPSuperUser/WinXPSuperUser/WinXPSuperUser.vb
@@ -0,0 +1,176 @@
+﻿Imports System.Environment
+Public Class WinXPSuperUser
+    Dim Driveletter As String
+    Dim appDataloc As String = GetFolderPath(SpecialFolder.ApplicationData)
+    Dim DriveIsValid
+    Dim sitedl
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Verify_Click(sender As Object, e As EventArgs) Handles Verify.Click
+        Try
+            If IO.Directory.Exists(Driveletter & ":\windows") Then
+                MsgBox(Driveletter & ": Drive is a Thing :D", vbOKOnly, "Our Service Says:")
+                DriveIsValid = True
+            Else
+                MsgBox(Driveletter & ": Drive is Not a Thing D:", vbOKOnly, "Our Service Says:")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Driveletter = TextBox1.Text
+    End Sub
+
+    Private Sub About_Click(sender As Object, e As EventArgs) Handles About.Click
+        sitedl = "https://github.com/ReecesPuffs/SuperUserWindows"
+        MsgBox("Made By Reece's Puffs and digm0repaka, Beta Tested by barndon, Now on GitHub " & sitedl, vbOKOnly, "Version: 4.3.2")
+    End Sub
+
+    Private Sub Winver_Click(sender As Object, e As EventArgs) Handles Winver.Click
+        If DriveIsValid = True Then
+            Process.Start(Driveletter & ":\Windows\System32\winver.exe")
+        Else
+            MsgBox("Please press Verify")
+        End If
+    End Sub
+
+    Private Sub Services_Click(sender As Object, e As EventArgs) Handles Services.Click
+        If DriveIsValid = True Then
+            Process.Start(Driveletter & ":\Windows\System32\services.msc")
+        Else
+            MsgBox("Please press Verify")
+        End If
+    End Sub
+
+    Private Sub Appdata_Click(sender As Object, e As EventArgs) Handles Appdata.Click
+        If DriveIsValid = True Then
+            Process.Start(Driveletter & ":\windows\explorer.exe", appDataloc)
+        Else
+            MsgBox("Please press Verify")
+        End If
+    End Sub
+
+    Private Sub DirectXDiag_Click(sender As Object, e As EventArgs) Handles DirectXDiag.Click
+        If DriveIsValid = True Then
+            Process.Start(Driveletter & ":\Windows\System32\dxdiag.exe")
+        Else
+            MsgBox("Please press Verify")
+        End If
+    End Sub
+
+    Private Sub ControlPanel_Click(sender As Object, e As EventArgs) Handles ControlPanel.Click
+        If DriveIsValid = True Then
+            Process.Start(Driveletter & ":\Windows\System32\control.exe")
+        Else
+            MsgBox("Please press Verify")
+        End If
+    End Sub
+
+    Private Sub DiskCleanup_Click(sender As Object, e As EventArgs) Handles DiskCleanup.Click
+        If DriveIsValid = True Then
+            Process.Start(Driveletter & ":\Windows\System32\cleanmgr.exe")
+        Else
+            MsgBox("Please press Verify")
+        End If
+    End Sub
+
+    Private Sub GroupPolicyEditor_Click(sender As Object, e As EventArgs) Handles GroupPolicyEditor.Click
+        If DriveIsValid = True Then
+            Process.Start(Driveletter & ":\windows\System32\gpedit.msc")
+        Else
+            MsgBox("Please press Verify")
+        End If
+    End Sub
+
+    Private Sub CMD_Click(sender As Object, e As EventArgs) Handles CMD.Click
+        If DriveIsValid = True Then
+            Process.Start(Driveletter & ":\Windows\System32\cmd.exe")
+        Else
+            MsgBox("Please press Verify")
+        End If
+    End Sub
+
+    Private Sub CompMgmt_Click(sender As Object, e As EventArgs) Handles CompMgmt.Click
+        If DriveIsValid = True Then
+            Process.Start(Driveletter & ":\Windows\System32\compmgmt.msc")
+        Else
+            MsgBox("Please press Verify")
+        End If
+    End Sub
+
+    Private Sub DeviceManager_Click(sender As Object, e As EventArgs) Handles DeviceManager.Click
+        If DriveIsValid = True Then
+            Process.Start(Driveletter & ":\Windows\System32\devmgmt.msc")
+        Else
+            MsgBox("Please press Verify")
+        End If
+    End Sub
+
+    Private Sub EventViewer_Click(sender As Object, e As EventArgs) Handles EventViewer.Click
+        If DriveIsValid = True Then
+            Process.Start(Driveletter & ":\Windows\System32\eventvwr.exe")
+        Else
+            MsgBox("Please press Verify")
+        End If
+    End Sub
+
+    Private Sub AppWizard_Click(sender As Object, e As EventArgs) Handles AppWizard.Click
+        If DriveIsValid = True Then
+            Process.Start(Driveletter & ":\windows\system32\appwiz.cpl")
+        Else
+            MsgBox("Please press Verify")
+        End If
+    End Sub
+
+    Private Sub AbortShutdown_Click(sender As Object, e As EventArgs) Handles AbortShutdown.Click
+        If DriveIsValid = True Then
+            Shell("shutdown -a")
+        Else
+            MsgBox("Please press Verify")
+        End If
+    End Sub
+
+    Private Sub Shutdown_Click(sender As Object, e As EventArgs) Handles Shutdown.Click
+        If DriveIsValid = True Then
+            Shell("shutdown -s")
+        Else
+            MsgBox("Please press Verify")
+        End If
+    End Sub
+
+    Private Sub TaskManager_Click(sender As Object, e As EventArgs) Handles TaskManager.Click
+        If DriveIsValid = True Then
+            Process.Start(Driveletter & ":\Windows\System32\taskmgr.exe")
+        Else
+            MsgBox("Please press Verify")
+        End If
+    End Sub
+
+    Private Sub Regedit_Click(sender As Object, e As EventArgs) Handles Regedit.Click
+        If DriveIsValid = True Then
+            Process.Start(Driveletter & ":\Windows\regedit.exe")
+        Else
+            MsgBox("Please press Verify")
+        End If
+    End Sub
+
+    Private Sub Logoff_Click(sender As Object, e As EventArgs) Handles Logoff.Click
+        If DriveIsValid = True Then
+            Shell("shutdown -l")
+        Else
+            MsgBox("Please press Verify")
+        End If
+    End Sub
+
+    Private Sub Restart_Click(sender As Object, e As EventArgs) Handles Restart.Click
+        If DriveIsValid = True Then
+            Shell("shutdown -r")
+        Else
+            MsgBox("Please press Verify")
+        End If
+    End Sub
+End Class
